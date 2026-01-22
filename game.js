ee929e4eedbe46edefd8d82333454224e1e0ad01
cnuttens <clement.nutt@gmail.com>
2026-01-22 15:28:05 +0100

    feat (main) : add Grace store

--- game.js
+++ game.js
@@ -1,9 +1,23 @@
+let strengthCost = 10;
 let player = {
   runes: 0,
   level: 1,
   vigor: 10,
   strength: 10,
   inventory: [],
+};
+
+const buyStrength = () => {
+  if (player.runes >= strengthCost) {
+    player.runes -= strengthCost;
+    player.strength += 5;
+    strengthCost = Math.floor(strengthCost * 1.5);
+
+    saveGame();
+    updateUI();
+  } else {
+    alert("Pas assez de runes !");
+  }
 };
 
 const saveGame = () => {
@@ -28,6 +42,13 @@ const updateUI = () => {
   document.getElementById("rune-count").innerText = player.runes;
   document.getElementById("level").innerText = player.level;
   document.getElementById("strength").innerText = player.strength;
+  document.getElementById("str-cost").innerText = strengthCost;
+
+  if (player.runes >= strengthCost) {
+    document.getElementById("buy-str").disabled = false;
+  } else {
+    document.getElementById("buy-str").disabled = true;
+  }
 };
 
 setInterval(saveGame, 30000);
